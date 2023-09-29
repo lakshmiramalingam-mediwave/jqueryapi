e7789ee7a9961591998ace1eea622d69087943b0
lakshmiramalingam-mediwave <lakshmi_ramalingam@mindwaveventures.com>
2023-09-29 11:58:39 +0530

    added style

--- app/script.js
+++ app/script.js
@@ -1,33 +1,59 @@
+$(document).ready(function() {
+  getAllDogsFromApi();
+  // button eruka id click intha  fun run agum
+  $("#getImg").on("click", function() {
+    const selectBreed = $("#dog-list");
+    $("#load-div").append('<p id="loading"><span>Loading ...</span></p>');
+    getRandomImageOfDog(selectBreed.val());
+  });
+
+
+  
+});
 function getAllDogsFromApi() {
-  // https://dog.ceo/api/breeds/list/all
   const url = "https://dog.ceo/api/breeds/list/all";
-  $.ajax(url, {
+  $.ajax({
+    url: url,
     method: "GET",
-    success: function (resp) {
+    success: function(resp) {
       console.log("Api request success");
-      const listOfDogs=Object.keys(resp.message);
-      console.log(listOfDogs);
+      const buttonshowlist = Object.keys(resp.message);
+      appendToSelect(buttonshowlist);
     },
-    error: function () {
+    error: function() {
       console.log("Api request error");
     },
-    complete: function () {
+    complete: function() {
       console.log("API request completed");
+
     },
   });
 }
+function appendToSelect(list) {
+  const selectBreed = $("#dog-list");
+  for (let item of list) {
+    const option = $("<option></option>").html(item);
+    selectBreed.append(option);
+  }
+}
+//image get
 function getRandomImageOfDog(dogBreed) {
-  // https://dog.ceo/api/breed/affenpinscher/images/random
   const url = `https://dog.ceo/api/breed/${dogBreed}/images/random`;
-  $.ajax(url, {
+  $.ajax({
+    url: url,
     method: "GET",
-    success: function (resp) {
+    success: function(resp) {
       console.log("Api request success");
-      console.log(resp);
+      console.log(resp.message);
+      $("#animals img").attr("src", resp.message);
+     
     },
-    error: function () {
+    error: function() {
       console.log("Api request error");
     },
+    complete: function() {
+      $("#loading").remove();
+    },
   });
 }
-getAllDogsFromApi();
+
